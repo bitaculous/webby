a88ab120f4ea7b6b9613f3dde15514a6ca90be58
Maik Kempe <mkempe@bitaculous.com>
2014-11-20 00:08:54 +0100

    [➦] Merged changes.

--- vendor/assets/javascripts/fss/sources/custom/renderers/canvas.js
+++ vendor/assets/javascripts/fss/sources/custom/renderers/canvas.js
@@ -4,9 +4,12 @@
  */
 FSS.CanvasRenderer = function() {
   FSS.Renderer.call(this);
+
   this.element = document.createElement('canvas');
   this.element.style.display = 'block';
+
   this.context = this.element.getContext('2d');
+
   this.setSize(this.element.width, this.element.height);
 };
 
@@ -14,15 +17,20 @@ FSS.CanvasRenderer.prototype = Object.create(FSS.Renderer.prototype);
 
 FSS.CanvasRenderer.prototype.setSize = function(width, height) {
   FSS.Renderer.prototype.setSize.call(this, width, height);
+
   this.element.width = width;
   this.element.height = height;
+
   this.context.setTransform(1, 0, 0, -1, this.halfWidth, this.halfHeight);
+
   return this;
 };
 
 FSS.CanvasRenderer.prototype.clear = function() {
   FSS.Renderer.prototype.clear.call(this);
+
   this.context.clearRect(-this.halfWidth, -this.halfHeight, this.width, this.height);
+
   return this;
 };
 
@@ -30,23 +38,26 @@ FSS.CanvasRenderer.prototype.render = function(scene) {
   FSS.Renderer.prototype.render.call(this, scene);
   var m,mesh, t,triangle, color;
 
-  // Clear Context
+  // Clear context
   this.clear();
 
-  // Configure Context
+  // Configure context
   this.context.lineJoin = 'round';
   this.context.lineWidth = 1;
 
-  // Update Meshes
+  // Update meshes
   for (m = scene.meshes.length - 1; m >= 0; m--) {
     mesh = scene.meshes[m];
+
     if (mesh.visible) {
       mesh.update(scene.lights, true);
 
-      // Render Triangles
+      // Render triangles
       for (t = mesh.geometry.triangles.length - 1; t >= 0; t--) {
         triangle = mesh.geometry.triangles[t];
+
         color = triangle.color.format();
+
         this.context.beginPath();
         this.context.moveTo(triangle.a.position[0], triangle.a.position[1]);
         this.context.lineTo(triangle.b.position[0], triangle.b.position[1]);
@@ -59,5 +70,6 @@ FSS.CanvasRenderer.prototype.render = function(scene) {
       }
     }
   }
+
   return this;
 };
--- vendor/assets/javascripts/fss/sources/custom/renderers/svg.js
+++ vendor/assets/javascripts/fss/sources/custom/renderers/svg.js
@@ -4,6 +4,7 @@
  */
 FSS.SVGRenderer = function() {
   FSS.Renderer.call(this);
+
   this.element = document.createElementNS(FSS.SVGNS, 'svg');
   this.element.setAttribute('xmlns', FSS.SVGNS);
   this.element.setAttribute('version', '1.1');
@@ -15,16 +16,20 @@ FSS.SVGRenderer.prototype = Object.create(FSS.Renderer.prototype);
 
 FSS.SVGRenderer.prototype.setSize = function(width, height) {
   FSS.Renderer.prototype.setSize.call(this, width, height);
+
   this.element.setAttribute('width', width);
   this.element.setAttribute('height', height);
+
   return this;
 };
 
 FSS.SVGRenderer.prototype.clear = function() {
   FSS.Renderer.prototype.clear.call(this);
+
   for (var i = this.element.childNodes.length - 1; i >= 0; i--) {
     this.element.removeChild(this.element.childNodes[i]);
   }
+
   return this;
 };
 
@@ -32,27 +37,33 @@ FSS.SVGRenderer.prototype.render = function(scene) {
   FSS.Renderer.prototype.render.call(this, scene);
   var m,mesh, t,triangle, points, style;
 
-  // Update Meshes
+  // Update meshes
   for (m = scene.meshes.length - 1; m >= 0; m--) {
     mesh = scene.meshes[m];
+
     if (mesh.visible) {
       mesh.update(scene.lights, true);
 
-      // Render Triangles
+      // Render triangles
       for (t = mesh.geometry.triangles.length - 1; t >= 0; t--) {
         triangle = mesh.geometry.triangles[t];
+
         if (triangle.polygon.parentNode !== this.element) {
           this.element.appendChild(triangle.polygon);
         }
-        points  = this.formatPoint(triangle.a)+' ';
-        points += this.formatPoint(triangle.b)+' ';
+
+        points  = this.formatPoint(triangle.a) + ' ';
+        points += this.formatPoint(triangle.b) + ' ';
         points += this.formatPoint(triangle.c);
+
         style = this.formatStyle(triangle.color.format());
+
         triangle.polygon.setAttributeNS(null, 'points', points);
         triangle.polygon.setAttributeNS(null, 'style', style);
       }
     }
   }
+
   return this;
 };
 
@@ -61,7 +72,9 @@ FSS.SVGRenderer.prototype.formatPoint = function(vertex) {
 };
 
 FSS.SVGRenderer.prototype.formatStyle = function(color) {
-  var style = 'fill:'+color+';';
-  style += 'stroke:'+color+';';
+  var style = 'fill:' + color + ';';
+
+  style += 'stroke:' + color + ';';
+
   return style;
 };
